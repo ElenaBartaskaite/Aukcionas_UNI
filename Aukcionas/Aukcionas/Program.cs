a97ee3a156b798d01b13eaa4e1ae11f64184455a
elenabart <elena.bartaskaite@gmail.com>
2016-12-12 17:41:52 +0200

    EntityFrameworkas padarytas

--- Aukcionas/Aukcionas/Program.cs
+++ Aukcionas/Aukcionas/Program.cs
@@ -14,7 +14,8 @@ namespace Aukcionas
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
+            using (var DB=new AukcionasEntities())
+                Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
