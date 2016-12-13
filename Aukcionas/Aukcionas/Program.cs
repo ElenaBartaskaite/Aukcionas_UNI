471efd3a8c4d0792778a48ee527e7d26627fe2c7
elenabart <elena.bartaskaite@gmail.com>
2016-12-13 14:41:55 +0200

    Duomenu ikelimo i db testavimas

--- Aukcionas/Aukcionas/Program.cs
+++ Aukcionas/Aukcionas/Program.cs
@@ -14,8 +14,18 @@ namespace Aukcionas
         [STAThread]
         static void Main()
         {
-            using (var DB=new AukcionasEntities())
-                Application.EnableVisualStyles();
+            AukcionasEntities db = new AukcionasEntities();
+            //db.Dailininkas.Remove(Dailininkas);
+            db.Dailininkas.Add(new Dailininkas { Vardas = "Petras", Pavarde = "Petrauskas", GimimoMetai = new DateTime(1995, 04, 04) });
+            db.SaveChanges();
+            IQueryable<string> dbInfo = db.Dailininkas.Select(x => x.Vardas + x.Pavarde);
+
+            foreach (string vardas in dbInfo)
+            {
+                Console.WriteLine(vardas);
+            }
+
+            Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
